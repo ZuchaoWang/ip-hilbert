ff8b1f340aadc1bd7fb5ee150d515650dbe8f68e
zuchao.wang <zuchao.wang@gmail.com>
2023-07-12 01:08:25 +0800

    quarts ...

---
+++ src/quart2hilbert.ts
@@ -0,0 +1,98 @@
+import { Rect, Square, convertSquareToRect } from "./region";
+
+// Helper lookup tables, by quart and angle
+const cosTable = [1, 0, -1, 0];
+const sinTable = [0, 1, 0, -1];
+const dxTable = [-1, -1, 1, 1];
+const dyTable = [-1, 1, 1, -1];
+const dangleTable = [-1, 0, 0, 1];
+const dflipTable = [-1, 1, 1, -1];
+
+/**
+ * Selects one of the four immediate children square regions from the parent square region `s`,
+ * based on the `quart`. This function mutates the input square `s` in-place.
+ *
+ * @param s - The parent square region.
+ * @param quart - The quart (0, 1, 2, or 3) used to select the child square region.
+ */
+function hilbertDrillDown(s: Square, quart: number) {
+  const ox = cosTable[s.angle];
+  const oy = sinTable[s.angle];
+  const dx = dxTable[quart];
+  const dy = dyTable[quart];
+  const dangle = dangleTable[quart];
+  const dflip = dflipTable[quart];
+
+  s.xc += (s.size / 4) * (dx * s.flip * ox - dy * oy);
+  s.yc += (s.size / 4) * (dx * s.flip * oy + dy * ox);
+  s.angle = (s.angle + dangle * s.flip + 4) % 4;
+  s.flip *= dflip;
+  s.size /= 2;
+}
+
+/**
+ * Selects one of the two immediate children rectangular regions from the parent square region `s`,
+ * based on the `quart`. The `quart` must be 0 or 2.
+ * This function creates a new rectangular region without modifying input square region.
+ *
+ * @param s - The parent square region.
+ * @param quart - The quart (0 or 2) used to select the child rectangular region.
+ */
+function hilbertDrillDownHalf(s: Square, quart: number): Rect {
+  const ox = cosTable[s.angle];
+  const oy = sinTable[s.angle];
+  const dx = quart / 2 ? 1 : -1;
+
+  const xc = s.xc + (s.size / 4) * (dx * s.flip * ox);
+  const yc = s.yc + (s.size / 4) * (dx * s.flip * oy);
+  const width = s.angle % 2 === 0 ? s.size / 2 : s.size;
+  const height = s.angle % 2 === 0 ? s.size : s.size / 2;
+  return { x: xc - width / 2, y: yc - height / 2, width, height };
+}
+
+/**
+ * Creates a descendant square region from a parent square region described by {x, y, size},
+ * based on the leading quarts.
+ *
+ * @param x - The x-coordinate of the center of the initial parent square region.
+ * @param y - The y-coordinate of the center of the initial parent square region.
+ * @param size - The size of the initial parent square region.
+ * @param leadingQuarts - The array of quarts used to select the descendant square region.
+ */
+export function hilbertQuartsToSquareRegion(
+  x: number,
+  y: number,
+  size: number,
+  leadingQuarts: number[]
+): Square {
+  const s: Square = { xc: x, yc: y, size: size, angle: 0, flip: 1 };
+  for (let i = 0; i < leadingQuarts.length; i++) {
+    hilbertDrillDown(s, leadingQuarts[i]);
+  }
+  return s;
+}
+
+/**
+ * Creates a descendant rectangular region from a parent square region described by {x, y, size},
+ * based on the leading quarts and last quart.
+ *
+ * @param x - The x-coordinate of the center of the initial parent square region.
+ * @param y - The y-coordinate of the center of the initial parent square region.
+ * @param size - The size of the initial parent square region.
+ * @param leadingQuarts - The array of quarts used to select the descendant square or rectangular region.
+ * @param lastQuart - The optional last quart (undefined, 0, or 2) used to further refine the selected rectangular region.
+ */
+export function hilbertQuartsToRectRegion(
+  x: number,
+  y: number,
+  size: number,
+  leadingQuarts: number[],
+  lastQuart?: number
+): Rect {
+  const s = hilbertQuartsToSquareRegion(x, y, size, leadingQuarts);
+  if (lastQuart === undefined) {
+    return convertSquareToRect(s);
+  } else {
+    return hilbertDrillDownHalf(s, lastQuart);
+  }
+}
---
+++ src/region.ts
@@ -0,0 +1,46 @@
+/**
+ * Interface for representing a square region in a 2D plane.
+ *
+ * @property {number} xc - The x-coordinate of the center of the square.
+ * @property {number} yc - The y-coordinate of the center of the square.
+ * @property {number} size - The full width and height of the square.
+ * @property {number} angle - The orientation angle of the square. It has values 0, 1, 2, or 3,
+ *                            which correspond to angles 0, PI/2, PI, and PI*3/2 respectively in a unit circle.
+ * @property {number} flip - The flip parameter. A value of 1 means no flipping, and -1 means the square is flipped.
+ */
+export interface Square {
+  xc: number;
+  yc: number;
+  size: number;
+  angle: number;
+  flip: number;
+}
+
+/**
+ * Interface for representing a rectangular region in a 2D plane.
+ *
+ * @property {number} x - The x-coordinate of the rectangle's minimum boundary.
+ * @property {number} y - The y-coordinate of the rectangle's minimum boundary.
+ * @property {number} width - The width of the rectangle.
+ * @property {number} height - The height of the rectangle.
+ */
+export interface Rect {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+/**
+ * Converts a square region to a rectangular region.
+ * @param {Square} s - The square region to be converted.
+ * @return {Rect} The resulting rectangular region.
+ */
+export function convertSquareToRect(s: Square): Rect {
+  return {
+    x: s.xc - s.size / 2,
+    y: s.yc - s.size / 2,
+    width: s.size,
+    height: s.size,
+  };
+}
