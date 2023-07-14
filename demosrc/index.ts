18b665bef4716501436608f89681be2fe51ecd48
zuchao.wang <zuchao.wang@gmail.com>
2023-07-14 09:40:00 +0800

    remove demosrc and webpack

--- demosrc/index.ts
+++
@@ -1,9 +0,0 @@
-import {add} from "../src";
-
-function startDemo() {
-  // Your code here
-  console.log("Window loaded: 1 + 2 = " + add(1, 2));
-  document.getElementById("content")!.innerHTML = "Window loaded: 1 + 2 = " + add(1, 2);
-}
-
-window.addEventListener("load", startDemo);
