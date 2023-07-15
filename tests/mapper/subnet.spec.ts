b7d329243776bd1615b4527830679f245a7eb478
zuchao.wang <zuchao.wang@gmail.com>
2023-07-15 12:35:08 +0800

    subnet test

--- tests/mapper/subnet.spec.ts
+++ tests/mapper/subnet.spec.ts
@@ -7,33 +7,48 @@ describe('SubnetHilbertMapper constructor input validation', () => {
   });
 
   it('should throw error when gridMaskLen is even', () => {
-      expect(() => new SubnetHilbertMapper(parsePrefix('192.0.2.0/24'), 27)).toThrow();
+    expect(() => new SubnetHilbertMapper(parsePrefix('192.0.2.0/24'), 27)).toThrow();
   });
 
   it('should throw error when gridMaskLen - subnetPrefix.maskLen > 15', () => {
-      expect(() => new SubnetHilbertMapper(parsePrefix('192.0.2.0/24'), 40)).toThrow();
+    expect(() => new SubnetHilbertMapper(parsePrefix('192.0.2.0/24'), 40)).toThrow();
   });
 
   it('should throw error when gridMaskLen < subnetPrefix.maskLen', () => {
-      expect(() => new SubnetHilbertMapper(parsePrefix('192.0.2.0/24'), 22)).toThrow();
+    expect(() => new SubnetHilbertMapper(parsePrefix('192.0.2.0/24'), 22)).toThrow();
   });
 });
 
 describe("SubnetHilbertMapper prefixToRectRegion for IPv4", () => {
   // ref: https://observablehq.com/@vasturiano/hilbert-map-of-ipv4-address-space
 
-  it("should have correct width and height", () => {
+  it("should have correct width and height under 0.0.0.0/0", () => {
     const subnetMapper = new SubnetHilbertMapper(parsePrefix("0.0.0.0/0"), 8);
     expect(subnetMapper.getWidth()).toBe(16);
     expect(subnetMapper.getHeight()).toBe(16);
   });
 
-  it("should map prefix to correct rect region", () => {
+  it("should map prefix to correct rect region under 0.0.0.0/0", () => {
     const subnetMapper = new SubnetHilbertMapper(parsePrefix("0.0.0.0/0"), 8);
-    expect(subnetMapper.prefixToRectRegion(parsePrefix("224.0.0.0/4"))).toEqual({x: 8, y:0, width: 4, height: 4});   // multicast
-    expect(subnetMapper.prefixToRectRegion(parsePrefix("49.0.0.0/8"))).toEqual({x: 2, y: 7, width: 1, height: 1});   // APNIC
-    expect(subnetMapper.prefixToRectRegion(parsePrefix("102.0.0.0/8"))).toEqual({x: 5, y: 15, width: 1, height: 1}); // AFRINIC
-    expect(subnetMapper.prefixToRectRegion(parsePrefix("186.0.0.0/7"))).toEqual({x: 12, y: 8, width: 2, height: 1}); // LACNIC
-    expect(subnetMapper.prefixToRectRegion(parsePrefix("202.0.0.0/7"))).toEqual({x: 12, y: 4, width: 1, height: 2}); // APNIC
+    expect(subnetMapper.prefixToRectRegion(parsePrefix("224.0.0.0/4"))).toEqual({ x: 8, y: 0, width: 4, height: 4 });  // multicast
+    expect(subnetMapper.prefixToRectRegion(parsePrefix("49.0.0.0/8"))).toEqual({ x: 2, y: 7, width: 1, height: 1 });   // APNIC
+    expect(subnetMapper.prefixToRectRegion(parsePrefix("102.0.0.0/8"))).toEqual({ x: 5, y: 15, width: 1, height: 1 }); // AFRINIC
+    expect(subnetMapper.prefixToRectRegion(parsePrefix("214.0.0.0/7"))).toEqual({ x: 8, y: 6, width: 2, height: 1 });  // US-DOD
+    expect(subnetMapper.prefixToRectRegion(parsePrefix("202.0.0.0/7"))).toEqual({ x: 12, y: 4, width: 1, height: 2 }); // APNIC
+  });
+
+  it("should have correct width and height under 192.0.0.0/3", () => {
+    const subnetMapper = new SubnetHilbertMapper(parsePrefix("192.0.0.0/3"), 8);
+    expect(subnetMapper.getWidth()).toBe(8);
+    expect(subnetMapper.getHeight()).toBe(4);
+  });
+
+  it("should map prefix to correct rect region under 192.0.0.0/3", () => {
+    const subnetMapper = new SubnetHilbertMapper(parsePrefix("192.0.0.0/3"), 8);
+    expect(subnetMapper.prefixToRectRegion(parsePrefix("224.0.0.0/4"))).toBeUndefined();                              // multicast
+    expect(subnetMapper.prefixToRectRegion(parsePrefix("49.0.0.0/8"))).toBeUndefined();                               // APNIC
+    expect(subnetMapper.prefixToRectRegion(parsePrefix("102.0.0.0/8"))).toBeUndefined();                              // AFRINIC
+    expect(subnetMapper.prefixToRectRegion(parsePrefix("214.0.0.0/7"))).toEqual({ x: 0, y: 2, width: 2, height: 1 }); // US-DOD
+    expect(subnetMapper.prefixToRectRegion(parsePrefix("202.0.0.0/7"))).toEqual({ x: 4, y: 0, width: 1, height: 2 }); // APNIC
   });
 });
